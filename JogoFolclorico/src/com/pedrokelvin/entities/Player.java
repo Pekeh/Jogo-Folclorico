6154edf0bcc0441d41f12573c99174dc5873d67d
Pedro <pedrinhoamarques85@gmail.com>
2019-07-11 08:25:03 -0300

    Primeira versao

---
+++ JogoFolclorico/src/com/pedrokelvin/entities/Player.java
@@ -0,0 +1,26 @@
+package com.pedrokelvin.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Player extends Entity{
+	
+	public boolean right, up, left, down;
+	public double speed = 2;
+
+	public Player(int x, int y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+	}
+	
+	public void tick() {
+		if(right)
+			x+=speed;
+		else if(left) 
+			x-=speed;
+		if(up)
+			y-=speed;
+		else if(down)
+			y+=speed;
+			
+	}
+
+}
